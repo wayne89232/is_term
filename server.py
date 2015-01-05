377079b4ada15e95265e0d05e375a3c585d1f029
wei2 <b01705015@ntu.edu.tw>
2015-01-05 16:18:12 +0800

    init

---
+++ server.py
@@ -0,0 +1,34 @@
+import socket
+import hashlib
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA 
+
+public_key = open('public_key', 'r').read().strip("'")[2:]
+# print(public_key)
+rsakey = RSA.importKey(public_key)
+rsakey = PKCS1_OAEP.new(rsakey)
+# encrypted = rsakey.encrypt(message)
+
+
+s = socket.socket()
+host = socket.gethostname()
+port = 30000
+s.bind((host, port))
+cert = "cert1"
+# cert = rsakey.encrypt(cert)
+print ("host on", host)
+
+s.listen(5)
+c, addr = s.accept()
+print ('Got connection from', addr)
+while True:
+	msg = c.recv(1024).decode()
+	if msg == "new patch":
+		print("new patch?")
+		c.send(cert.encode())
+	else:
+		recv = open("recv", "w")
+		recv.write(msg)
+		print("patch success")
+		break
+c.close()
